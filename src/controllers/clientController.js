4532c928d7c044c42f7b8e0ef2ddc7c2fbc7139a
Joshua Gocotano <gocotano.joshua02@gmail.com>
2025-03-31 15:39:50 +0800

    added the insert query in the services

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,8 +1,8 @@
-import * as clientServices from "../services/clientServices.js";
+import * as clientService from "../services/clientServices.js";
 
 export const getClients = async (req, res) => {
   try {
-    const client = await clientServices.getClients();
+    const client = await clientService.getClients();
     res.status(200).json(client);
   } catch (err) {
     console.error("Error fetching clients", err);
--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -4,3 +4,14 @@ export const getClients = async () => {
   const { rows } = await query("SELECT * FROM admin_tbl");
   return rows;
 };
+
+export const createClient = async (clientData) => {
+  const { fname, lname, email, passw, address, phone, dept, status } =
+    clientData;
+  const { rows } = await query(
+    "INSERT INTO admin_tbl (a_fname, a_lname, a_email, a_password, a_address, a_contactnum, a_department, a_status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+    [fname, lname, email, passw, address, phone, dept, status]
+  );
+
+  return rows[0];
+};
