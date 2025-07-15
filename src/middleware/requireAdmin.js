26be3d6548597394437647698770d02f51a66808
Joshua <gocotano.joshua02@gmail.com>
2025-07-15 21:00:45 +0800

    fix ginagmay

--- src/middleware/requireAdmin.js
+++ src/middleware/requireAdmin.js
@@ -1,7 +1,7 @@
 const requireAdmin = (req, res, next) => {
   if (req.user?.user_role !== "Admin") {
     return res.status(403).json({ error: "Access denied. Admins only." });
-  }
+  } 
   next();
 };
 
