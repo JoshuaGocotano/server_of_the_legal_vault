eaf7e02328596296a78931a69bc964f8fad2a3c3
Joshua Gocotano <152463576+JoshuaGocotano@users.noreply.github.com>
2025-07-19 01:32:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use("/api", branchRoutes);
 app.use("/api", userRoutes);
 app.use("/api", clientRoutes);
 app.use("/api", authRoutes); // authentication api
-app.use("/uploads", express.static("C:/Capstone/uploads")); // user profile uploads
+app.use("/uploads", express.static("D:/Capstone_ni_Angelie/uploads")); // user profile uploads
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
