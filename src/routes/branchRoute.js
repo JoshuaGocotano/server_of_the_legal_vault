c0908d438779b30712a1ab885cde7cd4675991c0
Joshua <gocotano.joshua02@gmail.com>
2025-07-23 16:03:22 +0800

    smol changes

--- src/routes/branchRoute.js
+++ src/routes/branchRoute.js
@@ -6,6 +6,6 @@ import requireAdmin from "../middleware/requireAdmin.js";
 
 const router = express.Router();
 
-router.get("/branches", branchController.getBranches);
+router.get("/branches", verifyUser, branchController.getBranches);
 
 export default router;
