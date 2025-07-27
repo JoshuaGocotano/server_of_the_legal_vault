44bdc51770cc3c637cdf7e0d8392019a6803dbb1
Joshua <gocotano.joshua02@gmail.com>
2025-07-27 15:56:31 +0800

    ... hala sige updatte

--- src/index.js
+++ src/index.js
@@ -27,6 +27,16 @@ app.use("/api", clientRoutes);
 app.use("/api", authRoutes); // authentication api
 app.use("/uploads", express.static("D:/Capstone_ni_Angelie/uploads")); // user profile uploads
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Listening on port ${port}`);
+});
+
+// Testing to get the IP address of the user
+app.get("/api/ip", (req, res) => {
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] || // for reverse proxies
+    req.socket?.remoteAddress ||
+    null;
+
+  res.json({ ip });
 });
