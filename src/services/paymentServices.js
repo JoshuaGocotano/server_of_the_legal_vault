97222f3d07691239c01c94ce60b1a380af475a48
Joshua <gocotano.joshua02@gmail.com>
2025-08-26 21:34:38 +0800

    payments api

--- src/services/paymentServices.js
+++ src/services/paymentServices.js
@@ -7,8 +7,11 @@ export const getPayments = async () => {
   const { rows } = await query(
     `SELECT *
         FROM payment_tbl p
-        JOIN case_tbl c ON p.case_id = c.case_id
-        JOIN user_tbl u ON p.user_id = u.user_id
+        LEFT JOIN case_tbl c ON p.case_id = c.case_id
+        LEFT JOIN user_tbl u ON p.user_id = u.user_id
+        LEFT JOIN client_tbl cl ON c.client_id = cl.client_id
+        LEFT JOIN case_category_tbl cc ON c.cc_id = cc.cc_id
+        LEFT JOIN cc_type_tbl ct ON c.ct_id = ct.ct_id
         ORDER BY p.payment_date DESC`
   );
   return rows;
